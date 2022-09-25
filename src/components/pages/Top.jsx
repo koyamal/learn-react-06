916f6db64e4a684d2315dffb918b83f5bed4d050
koyamal <74910401+koyamal@users.noreply.github.com>
2022-09-26 08:08:04 +0900

    Create button to move users page from top page

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,9 +1,22 @@
 import styled from "styled-components";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { useHistory } from "react-router-dom";
 
 export const Top = () => {
+  const history = useHistory();
+  const onClickAdmin = () => {
+    history.push({ pathname: "/users", state: { isAdmin: true } });
+  };
+  const onClickGeneral = () => {
+    history.push({ pathname: "/users", state: { isAdmin: false } });
+  };
   return (
     <SContainer>
       <h2>Top page!</h2>
+      <SecondaryButton onClick={onClickAdmin}>Admin user</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton onClick={onClickGeneral}>General user</SecondaryButton>
     </SContainer>
   );
 };
